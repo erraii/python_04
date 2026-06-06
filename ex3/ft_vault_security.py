bd333f292abae7610522735e04da617c036fa1b9
erraii <eraycakiray@gmail.com>
2026-06-07 00:47:05 +0200

    Update ft_vault_security.py

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -1,33 +1,36 @@
-def secure_archive(file_name: str, *options: str) -> tuple[bool, str]:
+def secure_archive(file_name: str, action: str = "r",
+                   content: str = "") -> tuple[bool, str]:
     try:
-        with open(file_name, options[0]) as file:
-            if options[0] == "r":
+        if action == "r":
+            with open(file_name, action) as file:
                 return (True, file.read())
-            elif options[0] == "w":
-                file.write(options[1])
+        elif action == "w":
+            with open(file_name, "w") as file:
+                file.write(content)
                 return (True, "Content successfully written to file")
+        return (False, "Invalid action")
     except Exception as e:
         return (False, f"{e}")
 
 
 def main() -> None:
     print("=== Cyber Archives Security ===")
-    print("Using 'secure_archive' to read from a nonexistent file:")
+    print("\nUsing 'secure_archive' to read from a nonexistent file:")
     file_name = "/not/existing/file"
     ret_tuple = secure_archive(file_name, "r")
-    print(f"{ret_tuple}\n")
-    print("Using 'secure_archive' to read from an inaccessible file:")
+    print(f"{ret_tuple}")
+    print("\nUsing 'secure_archive' to read from an inaccessible file:")
     file_name = "/etc/shadow"
     ret_tuple = secure_archive(file_name, "r")
-    print(f"{ret_tuple}\n")
-    print("Using 'secure_archive' to read from a regular file:")
+    print(f"{ret_tuple}")
+    print("\nUsing 'secure_archive' to read from a regular file:")
     file_name = "ancient_fragment.txt"
     ret_tuple = secure_archive(file_name, "r")
-    print(f"{ret_tuple}\n")
-    print("Using 'secure_archive' to write previous content to a new file:")
+    print(f"{ret_tuple}")
+    print("\nUsing 'secure_archive' to write previous content to a new file:")
     file_name = "new_file.txt"
     ret_tuple = secure_archive(file_name, "w", ret_tuple[1])
-    print(f"{ret_tuple}\n")
+    print(f"{ret_tuple}")
 
 
 if __name__ == "__main__":
