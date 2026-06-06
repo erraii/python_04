f46bb1034731191a4f14b8ebb1bf45da6da1156e
erraii <eraycakiray@gmail.com>
2026-06-06 18:15:38 +0200

    Add files via upload

---
+++ ft_ancient_text.py
@@ -0,0 +1,29 @@
+import sys
+import typing
+
+
+def main() -> None:
+    total_arguments = len(sys.argv)
+    if (total_arguments != 2):
+        print("Usage: ft_ancient_text.py <file>")
+        return
+    print("=== Cyber Archives Recovery ===")
+    file_name = sys.argv[1]
+    f: typing.IO[str] | None = None
+    try:
+        print(f"Accessing file '{file_name}'")
+        f = open(file_name)
+        print("---\n")
+        print(f.read())
+        print("---")
+    except Exception as e:
+        print(f"Error opening file '{file_name}': {e}")
+        return
+    finally:
+        if f is not None:
+            f.close()
+            print(f"File '{file_name}' closed.")
+
+
+if __name__ == "__main__":
+    main()
